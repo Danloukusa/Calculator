bfa66f47bcfbfa2b55c4a7a793f1e1c8857ce448
Danloukusa <Daniel.Loukusa@hotmail.com>
2020-08-08 09:35:48 -0400

    make buttons. make nums appear on screen. make grid.

--- calculator.js
+++ calculator.js
@@ -24,3 +24,27 @@ function operate(num1, operand, num2){
     if(operand == "/")
         return divide(num1,num2);
 }
+
+// I didn't feel like manually making each element. buuuut, seems like this was slower anyway.
+function fillCalculator(){
+    let box = document.getElementById("gridContainer");
+    let i = 0;
+    for(i = 0; i <= 9; i++)
+    {
+        let element = document.createElement("button");
+        element.id = i;
+        element.className = "Number";
+        element.innerText = i;
+        element.addEventListener("click", putOnScreen);
+        box.appendChild(element);
+    }
+}
+
+let displayVal = "";
+function putOnScreen(){
+    let screen = document.getElementById("display");
+    screen.innerText += this.id;
+    displayVal = screen.innerText;
+}
+
+fillCalculator();
